563b5000818d93b4b3dc59feed1cbd325e923e7b
donaldevine <donaldevine@gmail.com>
2013-10-04 11:12:12 +0100

    Initial Check-in
    Initial Check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClothesStore::Application.config.secret_token = '1797052aa65296e0edcf14f5d687f66dc6b346d66e6afbb0ddcd24201647b075b82d435effb0bb55300ac1527362ff57d95da47add33576cf6eb81c42e51f202'
